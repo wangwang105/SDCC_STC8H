32c390052a1eedcb4f8f3e63c67bc216b7ace6f5
王荣文 <1059451203@qq.com>
2022-07-21 15:44:18 +0800

    addfile

--- test/src/main.c
+++ test/src/main.c
@@ -7,18 +7,7 @@ void UART1Config(void);
 
 void main(void)
 {
-    // P0M1 = 0;
-    // P0M0 = 0;
-    // P1M1 = 0;
-    // P1M0 = 0;
-    // P2M1 = 0;
-    // P2M0 = 0;
-    // P3M1 = 0;
-    // P3M0 = 0;
-    // P4M1 = 0;
-    // P4M0 = 0;
-    // P5M1 = 0;
-    // P5M0 = 0;
+
     P0n_push_pull(Pin2);
 
     ClockConfig();
